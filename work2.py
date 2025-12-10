af9800a52a58eb598df98d8d2272109513d1c052
reena <nusreenanusri08@gmail.com>
2025-12-10 13:32:10 +0530

    update

---
+++ work2.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x + y
+
+
+def add(x,y):
+    return x + y
+
+
+
+def add(x,y):
+    return x + y
+
+
+
+def add(x,y):
+    return x + y
+
+
+
+def add(x,y):
+    return x + y
